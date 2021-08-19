0c1fd8b0f6c6e715e8b3cef4c553eed2b62190e0
Mattg0392 <78047262+Mattg0392@users.noreply.github.com>
2021-08-18 23:52:37 -0400

    added file path designation, and set up dictionary keys

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -1,4 +1,24 @@
 # Analyze election data
 import os
 import csv
+#set local path to csv file
+election_csv = os.path.join("..", "..", "election_data.csv")
+#create dic
+votes = {}
+#read csv
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    for row in csvreader:
+        candidate = row[2]
+        #if unique candidate add it, if it does exist, add vote.
+        if candidate not in votes:
+            votes[candidate] = 1
+        else:
+            votes[candidate] += 1
+
+#establish dictionary keys
+values = votes.values()
+voteTotal = sum(values)
+#output vote total
 
